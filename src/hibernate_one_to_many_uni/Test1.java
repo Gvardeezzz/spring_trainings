bb3ba16cb9e8cf3a5c6a2e054249347a49e00cc1
Gvardeezzz <93467736+Gvardeezzz@users.noreply.github.com>
2022-04-03 13:57:50 +0300

    1

---
+++ src/hibernate_one_to_many_uni/Test1.java
@@ -0,0 +1,69 @@
+package hibernate_one_to_many_uni;
+
+import hibernate_one_to_many_uni.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Department.class)
+                .buildSessionFactory();
+
+        Session session = null;
+
+        try {
+//            session = factory.getCurrentSession();
+//            Department dep = new Department("HR", 300, 1200);
+//            Employee emp1 = new Employee("Sergey", "Popov", 800);
+//            Employee emp2 = new Employee("Shok", "Rebeshok", 1000);
+//            dep.addEmployeeToDepartment(emp1);
+//            dep.addEmployeeToDepartment(emp2);
+//
+//            session.beginTransaction();
+//
+//            session.save(dep);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+
+//            session = factory.getCurrentSession();
+//
+//            session.beginTransaction();
+//
+//            Department department = session.get(Department.class,12);
+//            System.out.println(department);
+//            System.out.println(department.getEmps());
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+//
+//            session = factory.getCurrentSession();
+//
+//            session.beginTransaction();
+//
+//            Employee employee = session.get(Employee.class, 1);
+//            System.out.println(employee);
+//            System.out.println(employee.getDepartment());
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+//**********************************************************************
+            session = factory.getCurrentSession();
+
+            session.beginTransaction();
+
+            Department department = session.get(Department.class, 14);
+            session.delete(department);
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        }
+        finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
